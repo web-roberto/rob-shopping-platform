ece8dd1abaf3dd00a3d5edc73921d9aec9a8b097
web-roberto <roberto.alonso.gandia@gmail.com>
2025-02-19 17:39:31 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+export const metadata ={
+  title:'Home',
+}
+const Homepage = () => {
+  return (
+    <div>
+      Shopping Platform
+    </div>
+  )
+}
+
+export default Homepage
---
+++ lib/constants/index.tsx
@@ -0,0 +1,3 @@
+export const APP_NAME=process.env.NEXT_PUBLIC_APP_NAME ||'Shopping Plaformt';
+export const APP_DESCRIPTION=process.env.NEXT_PUBLIC_APP_DESCRIPTION ||"A modern shopping platform made easy to use and made with Next.js"
+export const SERVER_URL= process.env.NEXT_PUBLIC_SERVER_URL ||'http://localhost:3000'
